847bd409ebd028aa7aa4c9948e16b3a5b49fc65f
Nma Shawkat <nmashawket@gmail.com>
2022-12-05 22:40:16 +0300

    dooonnee

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData( name, email ) {
+    // this is to send data
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+    // then handling response
+      .then( function ( response ) {
+        return response.json()
+      } )
+    // retrieves the new id value and appends it to the DOM
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+    // handling the errors
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
